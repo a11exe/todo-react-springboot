94c6a6ce195803bb7e9c3d3e54cd07697bae1447
a.abramov <robobob8>
2021-01-05 15:55:43 +0300

    added constants

--- frontend/todo-app/src/components/todo/AuthenticationService.js
+++ frontend/todo-app/src/components/todo/AuthenticationService.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
+import { API_URL } from '../../Constants'
+
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
+
 
 class AuthenticationService {
 
     executeBasicAuthenticationService(username, password) {
-        return axios.get('http://localhost:8080/basicauth',
+        return axios.get(`${API_URL}/basicauth`,
             {headers: {authorization: this.createBasicAuthToken(username,password)}})
     }
 
     executeJwtAuthenticationService(username, password) {
-        return axios.post('http://localhost:8080/authenticate', {
+        return axios.post(`${API_URL}/authenticate`, {
             username,
             password
         })
@@ -23,7 +27,7 @@ class AuthenticationService {
     registerSuccessfulLogin(username,password){
         //let basicAuthHeader = 'Basic ' +  window.btoa(username + ":" + password)
         // console.log(basicAuthHeader)
-        sessionStorage.setItem('authenticatedUser', username)
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
         this.setupAxiosInterceptors(this.createBasicAuthToken(username,password))
     }
 
@@ -35,22 +39,22 @@ class AuthenticationService {
     registerSuccessfulLoginForJwt(username,token){
         //let basicAuthHeader = 'Basic ' +  window.btoa(username + ":" + password)
         // console.log(basicAuthHeader)
-        sessionStorage.setItem('authenticatedUser', username)
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
         this.setupAxiosInterceptors(this.createJwtAuthToken(token))
     }
 
     logout() {
-        sessionStorage.removeItem('authenticatedUser');
+        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
     }
 
     isUserLoggedIn() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
         if(user===null) return false
         return true
     }
 
     getLoggedInUserName() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
         if(user===null) return ''
         return user
     }
